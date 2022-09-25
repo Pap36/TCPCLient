6d5cff1accade1792f6ddd64d1c10f4fc1238417
Paul <pap36@bath.ac.uk>
2022-09-25 08:28:19 +0100

    Update Theme to Nordic Theme

--- app/src/main/java/com/example/tcpclient/MainActivity.kt
+++ app/src/main/java/com/example/tcpclient/MainActivity.kt
@@ -6,38 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tcpclient.ui.theme.TCPCLientTheme
+import com.example.tcpclient.screens.StartScreen
+import dagger.hilt.android.AndroidEntryPoint
+import no.nordicsemi.android.common.theme.NordicTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TCPCLientTheme {
+            NordicTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    StartScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TCPCLientTheme {
-        Greeting("Android")
     }
 }
